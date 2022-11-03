c15e8f80c22753e49bd898fbb292e472f5686c62
Andrey Suprano <andsuprano@gmail.com>
2022-11-03 12:56:14 -0300

    Troca telefone

--- components/TelefoneButton/TelefoneButton.tsx
+++ components/TelefoneButton/TelefoneButton.tsx
@@ -9,7 +9,7 @@ export const TelefoneButton = ({ variant }: TelefoneButtonProps) => {
 	return (
 		<S.Container>
 			<S.Text href="https://https://encurtador.com.br/bjknP" variant={variant ? variant : 'primary'}>
-				<FiPhone style={{ marginRight: 10 }} size={20} />(41) 99893-5960
+				<FiPhone style={{ marginRight: 10 }} size={20} />(41) 3248-2000
 			</S.Text>
 		</S.Container>
 	);
