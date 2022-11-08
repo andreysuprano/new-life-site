15f8ff8cec36b661f67f6550a1bac1d3f2c597e3
Andrey Suprano <andsuprano@gmail.com>
2022-11-08 12:02:40 -0300

    add ids

--- components/TopSection/TopSection.tsx
+++ components/TopSection/TopSection.tsx
@@ -14,6 +14,7 @@ export const TopSection = () => {
 						Cobrimos todos os procedimentos, para lhe dar conforto e velocidade no seu tratamento!
 					</S.Paragraph>
 					<S.Button
+						id="btn-whats"
 						onClick={() =>
 							sendToLink(
 								'https://api.whatsapp.com/send?phone=5541998935960&text=Ol%C3%A1%2C%20vim%20do%20site%20e%20gostaria%20de%20agendar%20uma%20avalia%C3%A7%C3%A3o%20e%20conhecer%20mais%20sobre%20a%20clinica!'
