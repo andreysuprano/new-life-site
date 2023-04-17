e3eefedfad870789b70dcd9f17d2928ecf461c58
Andrey Suprano <andsuprano@gmail.com>
2023-04-17 11:47:53 -0300

    href pages

--- components/CardServicoDetalhado/CardServicoDetalhado.tsx
+++ components/CardServicoDetalhado/CardServicoDetalhado.tsx
@@ -17,7 +17,7 @@ export const CardServicoDetalhado = ({ icon, title, text, href }: CardServicoPro
 					<S.Title>{title}</S.Title>
 				</S.CardHeader>
 				<S.Paragraph>{text}</S.Paragraph>
-				<S.Link id="btn-whats" href={'https://encurtador.com.br/bjknP'}>
+				<S.Link id="btn-whats" href={href}>
 					Ver mais
 				</S.Link>
 			</S.Content>
--- components/ServicoDetalhadoSection/ServicoDetalhadoSection.tsx
+++ components/ServicoDetalhadoSection/ServicoDetalhadoSection.tsx
@@ -20,13 +20,13 @@ export const ServicoDetalhadoSection = () => {
 					<CardServicoDetalhado
 						icon={ImplanteIcon}
 						title="Implantes & Próteses"
-						href="https://encurtador.com.br/bjknP"
+						href="implantes-proteses"
 						text="O implante dentário é utilizado com frequência nos consultórios, possibilitando a reposição do dente perdido. Mesmo necessitando de uma cirurgia prévia, eles apresentam a possibilidade de devolver uma ótima mastigação ao paciente."
 					/>
 					<CardServicoDetalhado
 						icon={ClareamentoDental}
 						title="Facetas & Lentes"
-						href="https://encurtador.com.br/bjknP"
+						href="facetas-lentes"
 						text="Elas corrigem problemas e imperfeições dos dentes como o fechamento de espaços entre dentes, pequenas correções na forma e cor de dentes anteriores ou até mesmo transformações estéticas mais marcantes do sorriso."
 					/>
 				</S.Divider>
@@ -34,13 +34,13 @@ export const ServicoDetalhadoSection = () => {
 					<CardServicoDetalhado
 						icon={AlinhadoresIcon}
 						title="Alinhadores Invisíveis"
-						href="https://encurtador.com.br/bjknP"
+						href="alinhadores-invisiveis"
 						text="Os alinhadores invisíveis, são extremamente estéticos e quase imperceptíveis. Na hora de comer, é só retirar o seu alinhador, guardar e pronto além de ser muito fácil para higienizar."
 					/>
 					<CardServicoDetalhado
 						icon={LaserIcon}
 						title="Harmonização Facial"
-						href="https://encurtador.com.br/bjknP"
+						href="implantes-proteses"
 						text="Temos uma variedade de procedimentos estéticos que promovem aos pacientes aumento da auto estima e confiança no dia dia, na New Life temos profissionais especializados em cada área de atuação ofertando os melhores resultados."
 					/>
 				</S.Divider>
