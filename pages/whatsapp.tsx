ea72d6cb0a1669070e520328c49007543c47a3fe
Andrey Suprano <andsuprano@gmail.com>
2023-08-17 17:55:30 -0300

    timer

--- pages/whatsapp.tsx
+++ pages/whatsapp.tsx
@@ -6,13 +6,13 @@ import { useEffect, useState } from 'react';
 import { sendToLink } from '../util/sendToLink';
 
 function Home() {
-	const [ count, setCount ] = useState(6);
+	const [ count, setCount ] = useState(3);
 	useEffect(() => {
 		setTimeout(() => {
 			sendToLink(
 				'https://api.whatsapp.com/send?phone=5541998935960&text=Ol%C3%A1%2C%20vim%20do%20site%20e%20gostaria%20de%20agendar%20uma%20avalia%C3%A7%C3%A3o%20e%20conhecer%20mais%20sobre%20a%20clinica!'
 			);
-		}, 5000);
+		}, 3000);
 	}, []);
 	setInterval(() => {
 		setCount(count - 1);
