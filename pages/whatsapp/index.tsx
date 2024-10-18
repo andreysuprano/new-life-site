69b9376b37c30cf478dada2fd223d8f498faf3cf
Andrey Suprano <54143276+andreysuprano@users.noreply.github.com>
2024-10-18 15:15:32 -0300

    Update index.tsx

--- pages/whatsapp/index.tsx
+++ pages/whatsapp/index.tsx
@@ -18,8 +18,8 @@ function Whats() {
 
 	useEffect(() => {
 		setTimeout(() => {
-			//const link = `https://api.whatsapp.com/send?phone=${phone}&text=Ol%C3%A1%2C%20vim%20do%20site%20e%20gostaria%20de%20agendar%20uma%20avalia%C3%A7%C3%A3o%20e%20conhecer%20mais%20sobre%20a%20clinica!`;
-			//sendToLink(link);
+			const link = `https://api.whatsapp.com/send?phone=${phone}&text=Ol%C3%A1%2C%20vim%20do%20site%20e%20gostaria%20de%20agendar%20uma%20avalia%C3%A7%C3%A3o%20e%20conhecer%20mais%20sobre%20a%20clinica!`;
+			sendToLink(link);
 		}, 1000);
 	}, []);
 	setInterval(() => {
@@ -32,7 +32,7 @@ function Whats() {
 				<S.ImageContainer>
 					<S.ImageTop src={Tony} alt="Dr. Tony Esper Jr" />
 				</S.ImageContainer>
-				<Image src={WhatsIcon} alt="" width="100" height="100" color="black"  />
+			
 				<Text textAlign="center" fontWeight="extrabold" fontSize="20px" marginTop="20px">
 					Estamos redirecionando você <br />para o WhatsApp!
 				</Text>
